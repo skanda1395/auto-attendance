2551618849479e3bf5d6af186015d7a5bdaf7bf9
Sunil <skanda1395@gmail.com>
2022-11-25 15:26:19 +0530

    Added cron functionality

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const puppeteer = require('puppeteer'); 
 const { HOST } = require("./config");
+const CronJob = require('cron').CronJob;
+const WEEKDAYS = "0 10 * * 1-5";
+const CUSTOM = "10 15 * * 1-5";
 require('dotenv').config();
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+async function signIn() {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  // Login
-  await page.goto(HOST);
-  await page.waitForSelector("#username");
-  await page.type("#username", process.env.AA_USERNAME);
-  await page.type("#password", process.env.AA_PASSWORD);
-  await page.click('[type="submit"]');
+    // Login
+    await page.goto(HOST);
+    await page.waitForSelector("#username");
+    await page.type("#username", process.env.AA_USERNAME);
+    await page.type("#password", process.env.AA_PASSWORD);
+    await page.click('[type="submit"]');
 
-  // Sign In / Sign Out
-  await page.waitForSelector('div.btn-container');
-  await page.$eval('gt-attendance-info gt-button', btn => btn.click());
+    // Sign In / Sign Out
+    await page.waitForSelector('div.btn-container');
+    await page.$eval('gt-attendance-info gt-button', btn => btn.click());
+  } catch(err) {
+    console.log("Error:", err);
+  }
+};
 
-})();
+const job = new CronJob(WEEKDAYS, signIn, null, false, "Asia/Kolkata");
+job.start();
