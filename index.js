4655a4d1964b420346ded503235ace474708cc37
Sunil <skanda1395@gmail.com>
2022-11-25 14:44:02 +0530

    sign-in/signout

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ require('dotenv').config();
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
+  // Login
   await page.goto(HOST);
   await page.waitForSelector("#username");
   await page.type("#username", process.env.AA_USERNAME);
   await page.type("#password", process.env.AA_PASSWORD);
   await page.click('[type="submit"]');
+
+  // Sign In / Sign Out
+  await page.waitForSelector('div.btn-container');
+  await page.$eval('gt-attendance-info gt-button', btn => btn.click());
+
 })();
